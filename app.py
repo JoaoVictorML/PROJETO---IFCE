6389d80253391d657b16ced75e9ce9671a286c25
João Victor Martins Lima. <joaovictorm605@gmail.com>
2026-05-17 21:21:41 -0300

    atualização 2

--- app.py
+++ app.py
@@ -8,11 +8,11 @@ def index():
     # O Flask vai procurar o arquivo index.html automaticamente dentro da pasta 'templates'
     return render_template('index.html')
 
-# 2. Rota para a Página de Cadastro (usuarios/cadastrar.html)
-@app.route('/cadastrar')
-def cadastrar():
+# 2. Rota para a Página de Dashboard (dashboard.html)
+@app.route('/dashboard')
+def dashboard():
     # Se o arquivo estiver dentro de uma subpasta, basta passar o caminho relativo
-    return render_template('usuarios/cadastrar.html')
+    return render_template('dashboard.html')
 
 # Passo fundamental para rodar o servidor local
 if __name__ == '__main__':
